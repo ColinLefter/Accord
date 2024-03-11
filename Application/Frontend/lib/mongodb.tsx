891b9e5bbc98e284f856c2164a0e2acaca6c2e5f
Colin Lefter <68645918+ColinLefter@users.noreply.github.com>
2024-03-10 17:27:58 -0700

    Revert "Completed Application Shell"

--- Application/Frontend/lib/mongodb.tsx
+++ Application/Frontend/lib/mongodb.tsx
@@ -1,27 +1,7 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 import { getMongoDbUri } from "@/lib/dbConfig";
 
-/**
- * This module establishes a connection to MongoDB using the MongoDB Node.js driver. It defines
- * a `run` function to test the connection by pinging the MongoDB server. The MongoClient is
- * configured to use a specific server API version, enabling strict mode and deprecation errors
- * to ensure that the application uses only the stable API features.
- *
- * @module mongodbConnection
- * @function run - Executes a ping command to test the MongoDB connection.
- * 
- * Usage:
- * - Import the `run` function from this module to test the MongoDB connection.
- * - The MongoClient instance is pre-configured with connection options for stable API usage.
- * - Once imported, invoking `run` will attempt to connect to the database and ping it,
- *   logging a success message or any errors encountered.
- *
- * Note:
- * - The MongoDB URI is retrieved from a separate configuration module to abstract away
- *   sensitive information and centralize configuration management.
- * - The `run` function is designed to be self-contained for easy testing and can be
- *   integrated into larger application flows to confirm database connectivity.
- */
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(getMongoDbUri(), {
   serverApi: {
     version: ServerApiVersion.v1,
