fdffef226cdafa7aa97ed4f8309b6e67f446c2c9
Hocng7 <hocng777@gmail.com>
2024-03-16 20:18:53 -0700

    FriendsTab Component is now completed with database implementation. Note: Search bar is still a WIP

--- Application/Frontend/components/FriendsColumn/FriendsTab.tsx
+++ Application/Frontend/components/FriendsColumn/FriendsTab.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Avatar, Group, Text, TextInput, TextInputProps, ActionIcon, useMantineTheme, Stack, Paper  } from '@mantine/core';
 import { IconSearch, IconArrowRight } from '@tabler/icons-react';
+import { FormEvent, useEffect, useState } from "react";
+import Link from "next/link";
+import { useRouter } from 'next/router';
 
 /**
  * FriendsTab provides a dedicated section within the application for displaying and interacting with
@@ -17,7 +20,98 @@ import { IconSearch, IconArrowRight } from '@tabler/icons-react';
  * 
  * @returns The JSX element representing the friends tab section, including a search bar and a list of friends.
  */
+
+  // Function to call to go back to the last previous URL
+  function goBack() {
+    window.history.back();
+  }
+
 export function FriendsTab(props: TextInputProps) {
+        const router = useRouter(); // To handle page redirection after the user logs in
+        const [friendList, setFriendList] = useState<string[]>([]);
+
+    // State hooks for form data and validation errors
+    const [formData, setFormData] = useState<{userName: string, password: string}>({
+        userName: "user1",
+        password: "user1Pass",
+      });
+    
+      // To facilitate client-side validation
+      const [usernameError, setUsernameError] = useState(''); // The message for an incorrect username
+      const [passwordError, setPasswordError] = useState(''); // The message for a correct username but incorrect password
+
+  
+      // Redirect you to the Modifying webpage
+      function onModify() {
+        router.push('create-account');
+      }
+    
+      /**
+       * Handles form submission, sending the login request to the server and processing
+       * the response to either proceed to the application or show error messages.
+       *
+       * @param event - The form submission event
+       */
+      const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+    
+        const response = await fetch('/api/login', { // Establishing a promise
+          method: 'POST', // As we are dealing with authentication, this is the most appropriate method
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData), // The data that is being sent
+        });
+    
+        const data = await response.json(); // Awaiting the resolution of the promise
+    
+        if (response.ok) { // If the response was successful
+          router.push('/accord'); // Redirect the user to the main application page
+        } else {
+          if (data.error === 'Invalid username') {
+            setUsernameError('Invalid username. Please try again.'); // Client-side validation
+          } else if (data.error === 'Invalid password') {
+            setPasswordError('Invalid password. Please try again.'); // Client-side validation
+          }
+        }
+      };
+  
+      /**
+       * Updates form data state on user input.
+       *
+       * @param evt - The input change event
+       */
+      const handleChange = (evt: any) => { // Upon changes, we update the form data
+        const { name, value } = evt.target;
+        setFormData(prevData => ({ ...prevData, [name]: value }));
+      };
+
+    
+    
+    useEffect(() => { 
+        const fetchData = async () => { //
+          try {
+            const response = await fetch('/api/FriendsTab',{ // Establishing a promise, replace userSettings with your API Endpoint file
+              method: 'POST', 
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(formData), 
+            }) // The data that is being sent);
+            if (response.ok) {  // If the Endpoint API code successfully ran, proceed 
+              const data = await response.json(); // 
+              setFriendList(data.friendList); // Set data accordingly
+
+            } else { // Error handling
+              console.error('Failed to fetch user settings');
+            }
+          } catch (error) {
+            console.error('Error fetching user settings:', error);
+          }
+        };
+        fetchData(); 
+      }, []);
+    
     return (
       <Stack>
           {/** Search Box*/}
@@ -45,16 +139,15 @@ export function FriendsTab(props: TextInputProps) {
           </Text>
           {/** Users that are in the friendlist*/}
           {
-              Array(10)
-                  .fill(0)
-                  .map((_, index) => (
+              friendList
+                  .map((friend, index) => (
                     <Paper color="black" shadow="xs" p="xs" radius="md" key={`friend-${index}`}>
                       <Group py="10" >
                           <Avatar
                               alt={`Friend ${index + 1}`}
                               radius="xl"
                           />
-                          <Text size="sm">Friend {index + 1}</Text>
+                          <Text size="sm">{friend}</Text>
                       </Group>
                     </Paper>
                   ))
