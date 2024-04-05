8f74409aca70797b7fbb57708c3890eb8b88bd5b
Colin Lefter <colin.lefter02@gmail.com>
2024-04-05 11:50:05 -0700

    Deleted unused backend folder, updated outline for dark theme

--- Application/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"a5bJ49UO0oVm0oexsSvXocm3GCLGmreoGOdiiDB/hX4=\"\n}"
--- Application/.next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/accord":["static\u002Fchunks\u002Fpages\u002Faccord.js"],sortedPages:["\u002F_app","\u002F_error","\u002Faccord"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
