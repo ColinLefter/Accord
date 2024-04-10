e1c55405d5c9d9304fbffb389e00f7dd97c46968
notbaopham <113058628+notbaopham@users.noreply.github.com>
2024-04-09 20:18:45 -0700

    Added API admin-promotion

---
+++ Application/pages/api/admin-promotion.ts
@@ -0,0 +1,46 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { MongoClient } from 'mongodb';
+import { getMongoDbUri } from '@/lib/dbConfig';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method !== 'POST') {
+      return res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+    
+    const { promotingID, promoterID, channelKey } = req.body;
+    const client = new MongoClient(getMongoDbUri());
+
+    try {
+
+      // Communicate with the server
+      await client.connect();
+      const db = client.db('Accord');
+      const chatsCollection = db.collection('Chats');
+
+      // Find entries that the member already exists in, or is already the owner of the server
+      const promotingIsAdmin = await chatsCollection.findOne({ channelKey : channelKey })
+      const promotingIsOwner = await chatsCollection.findOne({ channelKey : channelKey })
+
+
+      if (promotingIsAdmin) {
+        return res.status(201).json({ error: 'The user is already an admin of the text channel'})
+      }
+
+      if (promotingIsOwner) {
+        return res.status(202).json({ error: 'The user is the owner of the text channel'})
+      }
+
+      if (promoterID == promotingID ) {
+        return res.status(203).json({ error: 'You cant grant yourself more admin-ness' })
+      }
+
+      await chatsCollection.updateOne({ channelKey: channelKey}, {$addToSet: { adminsID: promotingID }})
+      return res.status(200).json({ message: 'Promoted user to admin successfully' });
+
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal server error' });
+    } finally {
+      await client.close();
+    }
+}
